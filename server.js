b65e56582248141d1978636f59c82b87da78c283
saumyadiphih7 <saumyadip@hih7.com>
2025-04-09 13:13:59 +0530

    test url added

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 import mongoose from "mongoose";
 import express from "express";
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import errorHandler from "./helper/errorHandler.js";
 import cookieParser from "cookie-parser";
 
+dotenv.config();
 
-dotenv.config()
+const app = express();
 
+connectDB();
 
-const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-connectDB()
+import adminRoute from "./routes/adminRoute.js";
+import panelRoute from "./routes/panelRoute.js";
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use("/api/admin", adminRoute);
+app.use("/api/panel", panelRoute);
 
-import adminRoute from "./routes/adminRoute.js"
-import panelRoute from "./routes/panelRoute.js"
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.use("/api/admin", adminRoute)
-app.use("/api/panel", panelRoute)
-
-app.use(errorHandler)
-app.listen(process.env.PORT, () => {  
-
+app.use(errorHandler);
+app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
-})
+});
