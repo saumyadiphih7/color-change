f68caf62182165757f0e82d24ad7393e8510cdce
saumyadiphih7 <saumyadip@hih7.com>
2025-04-09 14:55:18 +0530

    cors added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import errorHandler from "./helper/errorHandler.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 
 dotenv.config();
 
@@ -14,6 +16,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 import adminRoute from "./routes/adminRoute.js";
 import panelRoute from "./routes/panelRoute.js";
